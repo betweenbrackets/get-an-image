c2fed2e1145f5c74891a09a6859d76f8bbb170dc
ruby <betwixtbrackets@gmail.com>
2022-02-01 12:06:08 -0800

    configured attributes to display on click

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
 // declare an asynch function expression
 const getImage = async function () {
     // enable asynchronous communication between this program and the API
@@ -10,8 +11,41 @@ const getImage = async function () {
         );
 // parse the returned data as JSON
     const images = await res.json();
+// log the JSON array of images
+    // console.log(images);
 
-    console.log(images);
+// get that function rolling!
+    selectRandomImage(images);
 };
-// get the function rolling!
-getImage();
+
+
+// generate a random image from the number of images in the array 
+// round to a whole number
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(
+        Math.random() * images.length
+    )
+    // console.log(randomIndex);
+
+// grab a single image from the array
+    const randomImage = images[randomIndex];
+    // console.log(randomImage); 
+    
+    displayImage(randomImage);
+};
+
+// access the object attributes to display
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+    // get that function rolling when the button is clicked!
+    getImage();
+})
+
+
