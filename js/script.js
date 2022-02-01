57de513dd7757032bbe0c57de817cbb1bb7410d9
ruby <betwixtbrackets@gmail.com>
2022-02-01 11:02:24 -0800

    set up the API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+// declare an asynch function expression
+const getImage = async function () {
+    // enable asynchronous communication between this program and the API
+    const res = await fetch(
+        "https://picsum.photos/v2/list?limit=100"
+        );
+// parse the returned data as JSON
+    const images = await res.json();
+
+    console.log(images);
+};
+// get the function rolling!
+getImage();
